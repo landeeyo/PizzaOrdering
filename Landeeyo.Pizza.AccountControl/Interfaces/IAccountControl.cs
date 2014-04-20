1b857aaf599f45e2a2723a858d2f963cb4026fea
Landeeyo <landeeyo@10g.pl>
2014-04-20 17:37:12 +0200

    Modified solutions structure, added account control interface and user model.

---
+++ Landeeyo.Pizza.AccountControl/Interfaces/IAccountControl.cs
@@ -0,0 +1,24 @@
+﻿using Landeeyo.Pizza.Common.Models.AccountControl;
+
+namespace Landeeyo.Pizza.AccountControl.Interfaces
+{
+    /// <summary>
+    /// Authorization interface - authorizatio
+    /// </summary>
+    public interface IAccountControl
+    {
+        /// <summary>
+        /// Authorizes user
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <param name="password"></param>
+        /// <returns>True if authorized, false if not</returns>
+        bool AuthorizeUser(string userName, string password);
+
+        /// <summary>
+        /// Creates account
+        /// </summary>
+        /// <returns>True if created successfully, false if not</returns>
+        void CreateAccount(User user);
+    }
+}
---
+++ Landeeyo.Pizza.Common/Models/AccountControl/User.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Landeeyo.Pizza.Common.Models.AccountControl
+{
+    /// <summary>
+    /// User model
+    /// </summary>
+    public class User
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public bool IsAuthorized { get; set; }
+    }
+}
