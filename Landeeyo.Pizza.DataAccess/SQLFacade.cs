544d667839356d75458a76a85d583604e3705bb7
Landeeyo <landeeyo@10g.pl>
2014-04-26 22:36:36 +0200

    Added unit test for SQL DataAccess interface.

---
+++ Landeeyo.Pizza.DataAccess/SQLFacade.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Landeeyo.Pizza.Common.Models.AccountControl;
+using Landeeyo.Pizza.DataAccessLayer.EntityConfig;
+
+namespace Landeeyo.Pizza.DataAccessLayer
+{
+    public class SQLFacade : IDataAccess
+    {
+        public User GetUserByLogin(string login)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int? AddUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Landeeyo.Pizza.Test/Unit/AccountControl/AccountControlSQLTest.cs
@@ -0,0 +1,60 @@
+﻿using Landeeyo.Pizza.AccountControl.Interfaces;
+using Landeeyo.Pizza.AuthorizationLayer.Interfaces.Implementations;
+using Landeeyo.Pizza.Common.Exceptions.AccountControl;
+using Landeeyo.Pizza.Common.Models.AccountControl;
+using Landeeyo.Pizza.DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Landeeyo.Pizza.Test.Unit.AccountControl
+{
+    public class AccountControlSQLTest
+    {
+        private IAccountControl _accountControl;
+        private IDataAccess _dataAccess;
+
+        public AccountControlSQLTest()
+        {
+            _accountControl = new SimpleAccountControl();
+            _dataAccess = new SQLFacade();
+        }
+
+        [Fact]
+        public void CreateAndAuthorizeUser()
+        {
+            string properLogin = "TestLogin";
+            string properPassword = "TestPassword";
+            string improperLogin = "dfjkhsdfk";
+            string improperPassword = "fdjhsdfkf";
+
+
+            //arrange
+            User properUser = new User()
+            {
+                Login = properLogin,
+                Password = properPassword
+            };
+
+            _accountControl.SetDataSource = _dataAccess;
+
+            //act
+            var result1 = _accountControl.CreateAccount(properUser).HasValue;
+            var result2 = _accountControl.AuthorizeUser(properLogin, properPassword);
+            var result3 = _accountControl.AuthorizeUser(improperLogin, improperPassword);
+
+            //assert
+            Assert.True(result1);
+            Assert.Throws<UserExistsException>(
+                delegate
+                {
+                    _accountControl.CreateAccount(properUser);
+                });
+            Assert.True(result2);
+            Assert.False(result3);
+        }
+    }
+}
