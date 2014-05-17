542eb0a78d0c8dcf166766a8dc3b93df69d78bd7
Landeeyo <landeeyo@10g.pl>
2014-05-17 14:26:39 +0200

    First approach to presentation layer - MVC first controller and view has been added.

---
+++ Landeeyo.Pizza/Controllers/AccountControlController.cs
@@ -0,0 +1,70 @@
+﻿using Landeeyo.Pizza.AccountControl.Interfaces;
+using Landeeyo.Pizza.AuthorizationLayer.Interfaces.Implementations;
+using Landeeyo.Pizza.Common.Models.AccountControl;
+using Landeeyo.Pizza.DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Landeeyo.Pizza.Controllers
+{
+    public class AccountControlController : Controller
+    {
+        IUnitOfWork _unitOfWork;
+        IDataAccess _dataAccess;
+        IAccountControl _controller;
+
+        public AccountControlController()
+        {
+            _unitOfWork = new UnitOfWork();
+            _dataAccess = new SQLFacade(_unitOfWork);
+            _controller = new SimpleAccountControl();
+            _controller.SetDataSource = _dataAccess;
+        }
+
+        //
+        // GET: /AccountControl/
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // POST: /AccountControl/
+        [HttpPost]
+        public ActionResult Index(string s)
+        {
+            return View();
+        }
+
+        //
+        // GET: /AccountControl/Register
+        [HttpGet]
+        public ActionResult Register(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+        
+        //
+        // POST: /AccountControl/Register
+
+        [HttpPost]
+        public ActionResult Register(User model)
+        {
+            if (ModelState.IsValid)
+            {
+                _controller.AddUser(model);
+                _controller.Save();
+            }
+
+            // If we got this far, something failed, redisplay form
+            ModelState.AddModelError("", "The user name or password provided is incorrect.");
+            return View(model);
+        }
+
+    }
+}
