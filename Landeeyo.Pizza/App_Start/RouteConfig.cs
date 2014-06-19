28da54e5847ce6a04c1141b13c59546eac477d96
Landeeyo <landeeyo@10g.pl>
2014-06-19 17:07:21 +0200

    First part of authorization view.

--- Landeeyo.Pizza/App_Start/RouteConfig.cs
+++ Landeeyo.Pizza/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Landeeyo.Pizza
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "AccountControl", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
